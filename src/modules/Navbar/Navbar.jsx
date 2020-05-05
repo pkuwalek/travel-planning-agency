eb37e6ebba5436ea021714c2d076082d1de9c31a
Paula Kuwałek <paula.kuwalek@gmail.com>
2020-05-05 15:43:27 +0200

    finish responsive navbar with burger menu

--- src/modules/Navbar/Navbar.jsx
+++ src/modules/Navbar/Navbar.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+  const basicClassName = 'navbar';
+  const responsiveClassName = 'navbar responsive';
 
-    const burger = () => {
-        const x = document.getElementById('nav');
-        if (x.className === 'navbar') {
-          x.className += ' responsive';
-        } else {
-          x.className = 'navbar';
-        }
-      }
-  
-    return (
-      <div>
-          <div className='navbar' id='nav'>
-                <a href='#home'>Home</a>
-                <a href='#shop'>Shop</a>
-                <a href='#contact'>Contact</a>
-                <a href='#about'>About</a>
-                <a href='javascript:void(0);' className='icon' onClick={ burger() }>
-                    <FontAwesomeIcon className='burgerIcon' icon={faBars}/>
-                </a>
-          </div>
+  let [ activeClass, setActiveClass ] = useState(basicClassName);
+
+  const burger = () => {
+    setActiveClass(activeClass === basicClassName ? responsiveClassName : basicClassName);
+  }
+
+  return (
+    <div className="navContainer">
+      <h1 className='logo'>travel planning</h1>
+      <div className={ activeClass }>
+        <a href='#home'>Home</a>
+        <a href='#shop'>Shop</a>
+        <a href='#contact'>Contact</a>
+        <a href='#about'>About</a>
+        <a href='#' className='icon' onClick={ burger }>
+          <FontAwesomeIcon icon={ faBars }/>
+        </a>
       </div>
-    );
-  };
-  
-  export default Navbar;
+    </div>
+  );
+};
+
+export default Navbar;
