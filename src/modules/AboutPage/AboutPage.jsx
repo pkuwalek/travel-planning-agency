fd54f421f30b6a1cda98ae749ae00028771692c6
Paula Kuwałek <paula.kuwalek@gmail.com>
2020-06-13 11:22:54 +0200

    fix about page styling, make it responsive

--- src/modules/AboutPage/AboutPage.jsx
+++ src/modules/AboutPage/AboutPage.jsx
@@ -11,28 +11,32 @@ const AboutPage = () => {
                 <h1>Mia & Elena</h1>
             </div>
             <div className = 'content-container'>
-                <img src = {friends} />
-                <div>
-                    <p>
-                        Soufflé gummi bears icing topping liquorice apple pie. 
-                        Candy pie brownie. Muffin halvah sugar plum sesame snaps apple pie. 
-                        Jelly-o icing chocolate chocolate bar jujubes carrot cake. 
-                        Pie chocolate bar sweet cake jelly chocolate lollipop jelly-o. 
-                        <br />
-                        Topping chupa chups caramels cupcake wafer cupcake jelly beans bonbon.
-                        Soufflé gummi bears icing topping liquorice apple pie. 
-                        Candy pie brownie. Muffin halvah sugar plum sesame snaps apple pie. 
-                        Jelly-o icing chocolate chocolate bar jujubes carrot cake. 
-                        <br />
-                        Pie chocolate bar sweet cake jelly chocolate lollipop jelly-o. 
-                        Topping chupa chups caramels cupcake wafer cupcake jelly beans bonbon.
-                    </p>
-                    <button className = 'btn-reverse'>Come with us</button>
+                <div className = 'content'>
+                    <img src = {friends} />
                 </div>
-
+                <div className = 'content-p'>
+                    <div className = 'p-wrapper'>
+                        <p>
+                            Soufflé gummi bears icing topping liquorice apple pie. 
+                            Candy pie brownie. Muffin halvah sugar plum sesame snaps apple pie. 
+                            Jelly-o icing chocolate chocolate bar jujubes carrot cake. 
+                            Pie chocolate bar sweet cake jelly chocolate lollipop jelly-o. 
+                            <br />
+                            Topping chupa chups caramels cupcake wafer cupcake jelly beans bonbon.
+                            Soufflé gummi bears icing topping liquorice apple pie. 
+                            Candy pie brownie. Muffin halvah sugar plum sesame snaps apple pie. 
+                            Jelly-o icing chocolate chocolate bar jujubes carrot cake. 
+                            <br />
+                            Pie chocolate bar sweet cake jelly chocolate lollipop jelly-o. 
+                            Topping chupa chups caramels cupcake wafer cupcake jelly beans bonbon.
+                        </p>
+                    </div>
+                    <div>
+                        <button className = 'btn-reverse'>Come with us</button>
+                    </div>
+                </div>
             </div>
         </div>
-
     )
 }
 
