e109cc5c58c2c03839a92ba45ef01ac3156ab362
Paula Kuwałek <paula.kuwalek@gmail.com>
2020-06-14 12:34:24 +0200

    create draft of contact form

--- src/modules/ContactPage/ContactPage.jsx
+++ src/modules/ContactPage/ContactPage.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
 import Navbar from './../Navbar/Navbar';
+import ButtonReverse from './../ButtonReverse/ButtonReverse';
 import './contactpage.scss';
+import flatlaypassport from './../../flatlaypassport.jpg';
 
 const ContactPage = () => {
     return (
         <div>
             <Navbar />
-            <div>
-                <h1>Contact us</h1>
-                <p>name: first name, last name</p>
-                <p>email</p>
-                <p>subject</p>
-                <p>message</p>
+            <div className = 'contact-wrapper'>
+                <img src = { flatlaypassport } />
+                <form>
+                    <h1>Contact us</h1>
+                    <label>
+                        First Name
+                        <input type = 'text' />
+                    </label>
+                    <label>
+                        Last Name
+                        <input type = 'text' />
+                    </label>
+                    <label>
+                        Email
+                        <input type = 'email' />
+                    </label>
+                    <label>
+                        Subject
+                        <input type = 'text' />
+                    </label>
+                    <label>
+                        Message
+                        <textarea />
+                    </label>
+                    <ButtonReverse content = 'submit' />
+                </form>
             </div>
         </div>
-
     )
 }
 
